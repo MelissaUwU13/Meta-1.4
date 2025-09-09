de004be812f6ad84fdabdefa47e0a95e6be5adff
Melissa Rico <melissa.rico@uabc.edu.mx>
2025-09-09 10:59:30 -0700

    Ejercicio 1

--- estudiantes.js
+++ estudiantes.js
@@ -8,3 +8,49 @@ const estudiantes = [
 ];
 
 const calificaciones = [85, 92, 78, 90, 65, 88, 72, 95, 81, 79];
+
+function filtrarPorCarrera(obj) {
+  if ("carrera" in obj && obj.carrera == "Computación" ) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
+
+function filtrarPorPromedio(obj) {
+  if ("promedio" in obj && obj.promedio >= 8.5 ) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
+
+function filtrarPorMaterias(obj) {
+  if ("materiasAprobadas" in obj && obj.materiasAprobadas >= 30 ) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
+
+
+
+// 1.1. Obtener un array con solo los estudiantes de Computación
+const estudiantesComputacion = estudiantes.filter(filtrarPorCarrera);
+console.log("\nESTUDIANTES DE COMPUTACION\n");
+console.log("Array Filtrado\n", estudiantesComputacion);
+
+
+// 1.2. Obtener estudiantes con promedio mayor o igual a 8.5
+const estudiantesDestacados = estudiantes.filter(filtrarPorPromedio);
+console.log("\nESTUDIANTES DESTACADOS\n");
+console.log("Array Filtrado\n", estudiantesDestacados);
+
+
+// 1.3. Obtener estudiantes que hayan aprobado más de 30 materias
+console.log("\nESTUDIANTES AVANZADOS\n");
+const estudiantesAvanzados = estudiantes.filter(filtrarPorMaterias);
+console.log("Array Filtrado\n", estudiantesAvanzados);
